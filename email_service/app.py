e3fa5c6fb3ca27609dba413f236350f0557b0805
zemljanichka <zemljanichka2709@gmail.com>
2025-01-13 18:39:06 +0400

    feat: initial email_service

---
+++ email_service/app.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "OPTIONS", "PATCH", "DELETE", "PUT"],
+    allow_headers=["*"],
+)
+
+
+@app.post("/send_email")
+async def send_email():
+    pass
+
+
+@app.get("/read")
+async def read_email():
+    pass
