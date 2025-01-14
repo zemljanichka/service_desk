0fa537a64d1ecc8c86b95c026af20134a795222f
zemljanichka <zemljanichka2709@gmail.com>
2025-01-14 20:42:27 +0400

    feat: add assignment methods

--- api_service/services/assignment.py
+++ api_service/services/assignment.py
@@ -1,5 +1,5 @@
 from database import AssignmentModel
-from schemas import Assignment, OperatorResponse, Ordering, Status
+from schemas import Assignment, OperatorResponse, Ordering, Status, AssignmentEmail
 
 
 class AssignmentService:
@@ -15,15 +15,37 @@ class AssignmentService:
         return await AssignmentModel.select(order_by=order)
 
     @staticmethod
-    async def create_assignment(assignment_data: Assignment):
-        return await AssignmentModel.insert(**assignment_data.dict())
+    async def get_assignment_by_id(assignment_id: int):
+        return await AssignmentModel.get_single(AssignmentModel.id == assignment_id)
+
+    @staticmethod
+    async def create_assignment(assignment_data: dict):
+        return await AssignmentModel.insert(**assignment_data)
 
     @staticmethod
     async def take_assignment(assignment_id: int, operator: OperatorResponse):
+        assignment = await AssignmentModel.get_single(AssignmentModel.id == assignment_id)
+        if assignment.operator_id:
+            raise Exception
+        new_status = AssignmentService.get_new_status(assignment)
         return await AssignmentModel.update(
-            AssignmentModel.id == assignment_id, update_data={AssignmentModel.operator_id: operator.id}
+            AssignmentModel.id == assignment_id, update_data={AssignmentModel.operator_id: operator.id, AssignmentModel.status: new_status}
         )
 
     @staticmethod
-    async def change_status(assignment_id: int):
-        pass
+    async def change_status(assignment_id: int, operator: OperatorResponse):
+        assignment = await AssignmentModel.get_single(AssignmentModel.id == assignment_id)
+        if assignment.operator_id == operator.id:
+            new_status = AssignmentService.get_new_status(assignment)
+            return await AssignmentModel.update(AssignmentModel.id == assignment_id, update_data={AssignmentModel.status: new_status})
+        raise Exception
+
+    @staticmethod
+    def get_new_status(assignment: Assignment):
+        match assignment.status:
+            case Status.pending:
+                return Status.processing
+            case Status.processing:
+                return Status.closed
+            case Status.closed:
+                raise Exception
